10d745eec610a500d2c06792384ff41debcba34d
Faizan Ahmad <ahmadfaizan1604@gmail.com>
2020-10-19 18:46:27 +0530

    viewsets and routers

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
 from .views import GenericAPIView
+from django.urls import path, include
+from .views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
+ 
+ 
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
 
 urlpatterns = [
     path('article/<int:id>/', GenericAPIView.as_view()),
+    path('viewset/', include(router.urls))
 ]
